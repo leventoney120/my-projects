82c564418d45ce67bf7188a29f5561a52ae65c75
leventoney120 <flyoludeniz@gmail.com>
2022-10-24 16:38:31 +0300

    hatalar düzeltildi

--- python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-02-jinja-template/jinja.py
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-02-jinja-template/jinja.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def head():
-    return render_template('index.html', num1 = 7000, num2 = 9000)
+    return render_template('index.html', number1 = 7000, number2 = 9000)
 
 @app.route('/mult')
 def number():
-    var1, var2 = 30, 70
-    return render_template('body.html', num1 = var1, num2 = var2, multiplication = var1*var2)
+    var1, var2 = 3040, 7080
+    return render_template('body.html', number1 = var1, number2 = var2, multiplication = var1*var2)
 
 if __name__ == '__main__':
     app.run(debug=True)
